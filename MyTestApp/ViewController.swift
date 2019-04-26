f9ec44687222641eb60b7b8670af07e451176fe1
johnemile <john.t.emile@gmail.com>
2019-04-26 15:50:28 -0400

    Iniitial commit

---
+++ MyTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTestApp
+//
+//  Created by John Emile on 4/26/19.
+//  Copyright © 2019 LoveableApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
